4a25775b0a8bbb28d67cc7186b7bcd32d4a3cc45
Okta-jpg <oktavianuschn@gmail.com>
2025-02-08 22:19:50 +0700

    Feat: AddModelAI

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -33,7 +33,7 @@ const ChatPage = () => {
     setTextInput("");
 
     const stream = await ollama.chat({
-      model: "deepseek-r1:1.5b",
+      model: "deepseek-r1:8b",
       messages: [
         {
           role: "user",
